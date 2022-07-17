fad4bab6fa6425710d9f183205a5386ad3c9f84b
Dev.ThiagoJ <dev.thiagoj@gmail.com>
2022-07-17 15:32:57 -0300

    fix

--- Assets/Scripts/Traps/SwingTrap.cs
+++ Assets/Scripts/Traps/SwingTrap.cs
@@ -11,4 +11,12 @@ public class SwingTrap : MonoBehaviour
             if(PlayerController.Instance._isAlive == true) PlayerController.Instance.Dead();
         }
     }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.transform.CompareTag("Player"))
+        {
+            if (PlayerController.Instance._isAlive == true) PlayerController.Instance.Dead();
+        }
+    }
 }
